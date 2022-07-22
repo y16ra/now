b338321290f8386cc68f27433aa1c8c7173a83f8
y16ra <y16ra@users.noreply.github.com>
2022-07-22 09:20:27 +0900

    add flag "f"

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
+// Return the current local time
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
+var (
+	f = flag.String("f", "2006-01-02 15:04:05", "Date format strings like 2006-01-02 15:04")
+)
+
 func main() {
-	fmt.Println(time.Now())
+	flag.Parse()
+	fmt.Println(time.Now().Format(*f))
 }
